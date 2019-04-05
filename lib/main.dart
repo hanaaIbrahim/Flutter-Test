f67b8a14d091298f2ab133af0d19c7ca21aff68d
hanaaIbrahim <henootam@gmail.com>
2019-04-05 20:08:57 +0300

    Flutter Test

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:studyjamcode/helloflutter.dart';
+
+
+void main(){
+  runApp(MaterialApp(
+title: "Hello Flutter",
+home: HelloFlutter(),
+debugShowCheckedModeBanner: false,
+  )); 
+}
